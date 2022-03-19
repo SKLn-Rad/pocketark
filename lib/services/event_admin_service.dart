e491306263f274ed27cf3344372e175f67f31df3
Ryan Dixon <ryandixon1993@gmail.com>
2022-03-19 14:05:14 +0000

    Add logic

--- lib/services/event_admin_service.dart
+++ lib/services/event_admin_service.dart
@@ -1,5 +1,6 @@
 import 'package:inqvine_core_firebase/inqvine_core_firebase.dart';
 import 'package:inqvine_core_main/inqvine_core_main.dart';
+import 'package:pocketark/proto/events.pb.dart';
 import 'package:pocketark/services/event_service.dart';
 
 class EventAdminService extends InqvineServiceBase {
@@ -12,6 +13,7 @@ class EventAdminService extends InqvineServiceBase {
     // Get event data
 
     // Normalise event data
+    final List<LostArkEvent> events = <LostArkEvent>[];
 
     // Clear existing collection
     'Deleting old events'.logDebug();
@@ -21,5 +23,9 @@ class EventAdminService extends InqvineServiceBase {
     }
 
     // Add new events
+    for (final LostArkEvent event in events) {
+      final Object? json = event.toProto3Json();
+      await EventService.kEventCollection.add(json);
+    }
   }
 }
